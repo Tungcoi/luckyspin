76bd7b8ace1456494c88dfbc97d37b148468cf8c
Tung coi <Tnut.DCC@gmail.com>
2018-12-16 01:19:14 +0700

    final
    final

---
+++ AllWin/VongQuayMayMan/LuckySpiner/MainWindow.xaml.cs
@@ -0,0 +1,179 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace LuckySpiner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string bgPath;
+        public MainWindow()
+        {
+            InitializeComponent();
+            WindowStartupLocation = System.Windows.WindowStartupLocation.CenterScreen;
+            bgPath = "";
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bgPath = "";
+            var dlg = new OpenFileDialog();
+            dlg.Multiselect = true;
+            if (dlg.ShowDialog() == true)
+            {
+                List<String> imgs = new List<string>();
+                foreach (var path in dlg.FileNames)
+                {
+                    imgs.Add(path);
+                }
+
+                //testimg(imgs);
+                BackgroundWorker worker = new BackgroundWorker();
+                worker.DoWork += Worker_DoWork;
+                worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+                worker.ProgressChanged += Worker_ProgressChanged;
+                worker.WorkerReportsProgress = true;
+                worker.RunWorkerAsync(imgs);
+
+
+            }
+            
+        }
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            if (worker == null)
+                return;
+            switch (e.ProgressPercentage)
+            {
+                case 0:
+                    btn.Content = "Vui lòng chọn ảnh";
+                    bgPath = e.UserState.ToString();
+                    btn.IsEnabled = true;
+                    break;
+                case 1:
+                    btn.IsEnabled = false;
+                    btn.Content = "Đang phân tích ảnh đầu vào " + e.UserState.ToString();
+                    break;
+                case 2:
+                    btn.IsEnabled = false;
+                    btn.Content = "Đang ghép ảnh đầu vào thành ảnh nền.\nVui lòng đợi!!!";
+                    break;
+                case 3:
+                    btn.Content = "Đang ghi ảnh nền. Vui lòng đợi!!!";
+                    btn.IsEnabled = false;
+                    break;
+                case 4:
+                    btn.Content = "Đang khởi tạo cửa sổ mới. Vui lòng đợi!!!";
+                    btn.IsEnabled = false;
+                    break;
+                case 100:
+                    MessageBox.Show("Vui lòng kiểm tra lại ảnh:" + e.UserState.ToString());
+                    this.Close();
+                    break;
+            }
+                
+        }
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(bgPath))
+                return;
+            List<String> data = e.Result as List<String>;
+            SpinerWindow wd = new SpinerWindow(bgPath, data);
+            wd.Show();
+            this.Close();
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            if (worker == null)
+                return;
+            List<string> imgsPth = e.Argument as List<string>;
+            //make background image
+            int imgWidth = 3840;
+            int imgHeight = 2160;
+            int imgSize = 1;
+            while (imgSize * imgSize < imgsPth.Count) imgSize++;
+            int tileWidth = imgWidth / imgSize;
+            int tileHeight = imgHeight / imgSize;
+            DrawingVisual drawingVisual = new DrawingVisual();
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+            {
+                for (int i = 0; i < imgSize; i++)
+                {
+                    for (int j = 0; j < imgSize; j++)
+                    {
+                        int pos = (i * imgSize + j) % imgsPth.Count;
+                        if ((i * imgSize + j) < imgsPth.Count) 
+                            worker.ReportProgress(1, String.Format(" - {0}({1}/{2})", System.IO.Path.GetFileName(imgsPth[pos]), pos + 1, imgSize * imgSize));
+                        string pth = imgsPth[pos];
+                        BitmapImage bi = new BitmapImage();
+                        bi.BeginInit();
+                        bi.DecodePixelWidth = tileWidth;
+                        bi.DecodePixelHeight = tileHeight;
+                        try
+                        {
+                            bi.UriSource = new Uri(pth);
+                            bi.EndInit();
+                        }
+                        catch (Exception ex)
+                        {
+                            string mydocpath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+                            // Append text to an existing file named "WriteLines.txt".
+                            using (StreamWriter outputFile = new StreamWriter(System.IO.Path.Combine(mydocpath, "LuckySpinerLog.txt"), true))
+                            {
+                                outputFile.WriteLine(DateTime.Now.ToString());
+                                outputFile.WriteLine(ex.Message);
+                                drawingContext.Close();
+                                worker.ReportProgress(100, pth);
+                                return;
+                            }
+                        }
+                        drawingContext.DrawImage(bi, new Rect(i * tileWidth + 1, j * tileHeight + 1, tileWidth - 2, tileHeight - 2));
+                        GC.Collect();
+                    }
+                }
+            }
+
+            worker.ReportProgress(2);
+            RenderTargetBitmap bmp = new RenderTargetBitmap(imgWidth, imgHeight, 96, 96, PixelFormats.Pbgra32);
+            bmp.Render(drawingVisual);
+
+            PngBitmapEncoder encoder = new PngBitmapEncoder();
+            encoder.Frames.Add(BitmapFrame.Create(bmp));
+
+            worker.ReportProgress(3);
+            string mypicpath = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures) + @"\LuckySpinerBackground.png";
+            using (Stream stream = File.Create(mypicpath))
+                encoder.Save(stream);
+
+            e.Result = imgsPth;
+            //done
+            worker.ReportProgress(0, mypicpath);
+        }
+
+
+    }
+}
---
+++ AllWin/VongQuayMayMan/LuckySpiner/SpinerWindow.xaml.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace LuckySpiner
+{
+    /// <summary>
+    /// Interaction logic for SpinerWindow.xaml
+    /// </summary>
+    public partial class SpinerWindow : Window
+    {
+
+        public static readonly DependencyProperty ScrollOffsetProperty =
+        DependencyProperty.Register("ScrollOffset", typeof(double), typeof(SpinerWindow),
+            new FrameworkPropertyMetadata(0.0, new PropertyChangedCallback(OnScrollOffsetChanged)));
+
+        public double ScrollOffset
+        {
+            get { return (double)GetValue(ScrollOffsetProperty); }
+            set { SetValue(ScrollOffsetProperty, value); }
+        }
+
+        private static void OnScrollOffsetChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            SpinerWindow myObj = obj as SpinerWindow;
+
+            if (myObj != null)
+                myObj.scroll.ScrollToHorizontalOffset(myObj.ScrollOffset);
+        }
+
+
+        private string mainPath;
+        private bool isRotate;
+
+        DoubleAnimation rotateAnimation;
+        Storyboard storyboard;
+        private int animationDuration;
+
+        private int btnMode;
+        private bool rotateMode;
+
+        DispatcherTimer countDownTimer;
+        private int countDownValue;
+
+        public SpinerWindow(String bg, List<String> imgsPth)
+        {
+            InitializeComponent();
+            BitmapImage bi = new BitmapImage(new Uri(bg));
+            background.ImageSource = bi;
+            lsImgInfo = new List<ImgIfo>();
+
+            foreach(var pth in imgsPth)
+            {
+                Image img = new Image();
+                BitmapImage bmi = new BitmapImage();
+                bmi.BeginInit();
+                bmi.DecodePixelHeight = 400;
+                bmi.UriSource = new Uri(pth);
+                bmi.EndInit();
+                img.Source = bmi;
+                lsStack.Children.Add(img);
+
+                ImgIfo info = new ImgIfo();
+                info.path = pth;
+                info.imgView = img;
+                lsImgInfo.Add(info);
+            }
+
+            mainPath = null;
+            isRotate = false;
+            scroll.ScrollChanged += Scroll_ScrollChanged;
+
+            rotateAnimation = new DoubleAnimation();
+            storyboard = new Storyboard();
+            storyboard.Children.Add(rotateAnimation);
+            Storyboard.SetTarget(rotateAnimation, this);
+            Storyboard.SetTargetProperty(rotateAnimation, new PropertyPath(SpinerWindow.ScrollOffsetProperty)); // Attached dependency property
+            //rotateAnimation.Completed += RotateAnimation_Completed;
+            rotateAnimation.AutoReverse = true;
+            rotateAnimation.RepeatBehavior = RepeatBehavior.Forever;
+            btnMode = 0;
+            rotateMode = true;//left to right
+            animationDuration = 5000;
+
+            countDownTimer = new DispatcherTimer();
+            countDownTimer.Tick += CountDownTimer_Tick;
+            countDownTimer.Interval = TimeSpan.FromSeconds(1);
+            countDownValue = 5;
+        }
+
+        private void CountDownTimer_Tick(object sender, EventArgs e)
+        {
+            countDownValue--;
+            Uri uri;
+            switch (countDownValue)
+            {
+                case 4://show 5 image
+                    uri = new Uri(@"pack://application:,,,/Resources/5.png");
+                    break;
+                case 3:
+                    uri = new Uri(@"pack://application:,,,/Resources/4.png");
+                    break;
+                case 2:
+                    uri = new Uri(@"pack://application:,,,/Resources/3.png");
+                    break;
+                case 1:
+                    uri = new Uri(@"pack://application:,,,/Resources/2.png");
+                    break;
+                case 0:
+                    uri = new Uri(@"pack://application:,,,/Resources/1.png");
+                    break;
+                case -1:
+                    btnMode = 0;
+                    animationDuration = 5000;
+                    isRotate = false;
+                    rotateBtn.Content = "Quay thưởng";
+                    storyboard.Stop();
+                    doAnimation(rotateMode, animationDuration);
+
+                    BackgroundWorker bw = new BackgroundWorker();
+                    bw.DoWork += Bw_DoWork1;
+                    bw.RunWorkerAsync(mainPath);
+
+                    countDownTimer.Stop();
+                    cdImg.Visibility = Visibility.Hidden;
+                    return;
+                default:
+                    countDownTimer.Stop();
+                    return;
+            }
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.DecodePixelHeight = 400;
+            bi.UriSource = uri;
+            bi.EndInit();
+            cdImg.Source = bi;
+            cdImg.Visibility = Visibility.Visible;
+        }
+
+        //private void RotateAnimation_Completed(object sender, EventArgs e)
+        //{
+        //    rotateMode = !rotateMode;
+        //    storyboard.Stop();
+        //    doAnimation(rotateMode, animationDuration);
+        //}
+
+        private void Scroll_ScrollChanged(object sender, ScrollChangedEventArgs e)
+        {
+            if (!isRotate)
+                return;
+            string newpth = getPath(e.HorizontalOffset);
+            if (newpth == null || string.Equals(newpth, mainPath))
+                return;
+            BackgroundWorker bw = new BackgroundWorker();
+            bw.DoWork += Bw_DoWork;
+            mainPath = newpth;
+            bw.RunWorkerAsync(newpth);
+
+        }
+
+        private void Bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            String newpth = e.Argument as String;
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.DecodePixelWidth = 600;
+            bi.UriSource = new Uri(newpth);
+            bi.EndInit();
+            bi.Freeze();
+            Dispatcher.Invoke(() => mainImg.Source = bi);
+        }
+
+        private string getPath(double horizontalOffset)
+        {
+            foreach (var item in lsImgInfo)
+            {
+                if (item.startOffset <= horizontalOffset && item.endOffset >= horizontalOffset && !item.isLucky)
+                {
+                    return item.path;
+                }
+            }
+            return null;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Show();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            calculatePosition();
+            doAnimation(rotateMode, animationDuration);
+        }
+
+        private bool isCalculate = false;
+        public class ImgIfo
+        {
+            public string path { set; get; }
+            public double startOffset { set; get; }
+            public double endOffset { set; get; }
+            public int position { set; get; }
+            public Image imgView { set; get; }
+            public bool isLucky { set; get; }
+        }
+
+        List<ImgIfo> lsImgInfo;
+        private void calculatePosition()
+        {
+            if (isCalculate)
+                return;
+            isCalculate = true;
+            double offset = 0;
+            for (int i = 0; i < lsStack.Children.Count; i++)
+            {
+                ImgIfo info = lsImgInfo[i];
+                info.startOffset = offset;
+                offset += info.imgView.ActualWidth;
+                info.endOffset = offset;
+                info.position = i;
+                info.isLucky = false;
+            }
+            double neededwidth = scroll.ViewportWidth;
+            int posadd = 0;
+            while (neededwidth > 0)
+            {
+                neededwidth -= lsImgInfo[posadd].imgView.ActualWidth;
+                Image img = new Image();
+                BitmapImage bi = new BitmapImage();
+                bi.BeginInit();
+                bi.DecodePixelHeight = 400;
+                bi.UriSource = new Uri(lsImgInfo[posadd].path);
+                bi.EndInit();
+                img.Source = bi;
+                lsStack.Children.Add(img);
+                posadd++;
+            }
+        }
+
+        private void doAnimation(bool mode, int tpp, double startOffset = -1)//time per page
+        {
+            int duration = (int)(scroll.ExtentWidth / scroll.ViewportWidth * tpp);
+            rotateAnimation.From = scroll.HorizontalOffset;
+            if (mode == true) //Left to right
+            {
+                if (startOffset == -1)
+                    rotateAnimation.From = 0;
+                rotateAnimation.To = scroll.ExtentWidth;
+            } else //Righ to left
+            {
+                if (startOffset == -1)
+                    rotateAnimation.From = scroll.ExtentWidth;
+                rotateAnimation.To = 0;
+            }
+
+            rotateAnimation.Duration = new Duration(TimeSpan.FromMilliseconds(duration));
+
+            storyboard.Begin();
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnMode == 0)
+            {
+                btnMode = 1;
+                animationDuration = 300;
+                isRotate = true;
+                rotateBtn.Content = "Ngừng quay";
+                storyboard.Stop();
+
+                doAnimation(rotateMode, animationDuration);
+            }
+            else
+            {
+                rotateBtn.IsEnabled = false;
+                countDownValue = 5;
+                countDownTimer.Start();
+            }
+           
+        }
+
+        private void Bw_DoWork1(object sender, DoWorkEventArgs e)
+        {
+            String newpth = e.Argument as String;
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.DecodePixelWidth = 600;
+            bi.UriSource = new Uri(newpth);
+            bi.EndInit();
+            bi.Freeze();
+            Dispatcher.Invoke(() => awardImg.Source = bi);
+            foreach(var item in lsImgInfo)
+            {
+                if (string.Equals(newpth, item.path))
+                {
+                    item.isLucky = true;
+                    break;
+                }
+            }
+            System.Threading.Thread.Sleep(2000);
+            Dispatcher.Invoke(() => awardImg.Visibility = Visibility.Visible);
+            Dispatcher.Invoke(() => winner.Visibility = Visibility.Visible);
+            BitmapImage bi2 = new BitmapImage();
+            bi2.BeginInit();
+            bi2.DecodePixelHeight = 400;
+            bi2.UriSource = new Uri(@"pack://application:,,,/Resources/logo.png");
+            bi2.EndInit();
+            bi2.Freeze();
+            Dispatcher.Invoke(() => mainImg.Source = bi2);
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                hideLucky();
+            }
+        }
+
+        private void hideLucky()
+        {
+            awardImg.Visibility = Visibility.Hidden;
+            winner.Visibility = Visibility.Hidden;
+            rotateBtn.IsEnabled = true;
+        }
+        private void awardImg_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left && e.ClickCount >= 2)
+            {
+                hideLucky();
+            }
+        }
+
+    }
+
+}
